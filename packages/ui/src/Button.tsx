587c19d8109e0cd1e19002fe5d32ce72a2b1f80b
Joey Figaro <joey@joeyfigaro.com>
2022-02-10 17:31:18 -0500

    feat: added UI package

---
+++ packages/ui/src/Button.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Button: React.FC<{
+  children?: React.ReactNode
+  onClick: (e?: any) => void
+}> = ({ children, onClick }) => {
+  return <button onClick={onClick}>{children}</button>
+}
