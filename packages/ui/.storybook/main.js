f0b20aefa4dc3cec33f9e2114a10ca8cd520fff1
Joey Figaro <joey@joeyfigaro.com>
2022-04-21 10:46:02 -0400

    feat: add storybook support/config

---
+++ packages/ui/.storybook/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+  stories: ['../src/**/stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/react',
+  webpackFinal: (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    })
+
+    return config
+  },
+}
