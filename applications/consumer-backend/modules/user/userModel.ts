e8c50f04b09771a7646bf899a0cc19b48a9c9ddb
Vigas Deep <vigasdeep@gmail.com>
2022-05-27 08:35:34 +0530

    removed unwanted fields in userModel

--- applications/consumer-backend/modules/user/userModel.ts
+++ applications/consumer-backend/modules/user/userModel.ts
@@ -2,22 +2,11 @@ import { Document, Model, model, Schema } from 'mongoose'
 
 export interface IUser extends Document {
   name: string
-  email: string
-  password: string
 }
 
 const userSchema: Schema = new Schema(
   {
     name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
       type: String,
       required: true,
     },
