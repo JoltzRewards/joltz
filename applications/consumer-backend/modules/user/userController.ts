8a5f5bc0062aac916d260df8aacfb804af4f8dfa
Joey Figaro <joey@joeyfigaro.com>
2022-06-02 16:01:33 -0400

    fix: remove implementation of user creation

--- applications/consumer-backend/modules/user/userController.ts
+++ applications/consumer-backend/modules/user/userController.ts
@@ -1,49 +1,19 @@
 'use strict'
 
-import { Request, Response, NextFunction } from 'express'
+import { Request, NextFunction } from 'express'
 import { StatusCodes, getReasonPhrase } from 'http-status-codes'
-import { OperationResponse } from '../../utils/responseHandler'
-
-import { User } from './userModel'
+import { OperationResponse } from '../../utils'
 
 /**
  * Creates a new User, and returns it as a JSON
  */
-export const create = async (req: Request, res: OperationResponse, next: NextFunction) => {
-  const data = req.body
-
-  if (Object.getOwnPropertyNames(data).length === 0) {
-    return res.json({
-      success: false,
-      status: StatusCodes.NO_CONTENT,
-      error: {
-        code: 'NO_CONTENT',
-        message: getReasonPhrase(StatusCodes.NO_CONTENT),
-      },
-    })
-  }
-
-  const { name } = req.body
-  const newUser = {
-    name,
-  }
-
-  await User.create(newUser)
-    .then((result) =>
-      res.json({
-        success: true,
-        status: StatusCodes.CREATED,
-        data: result,
-      }),
-    )
-    .catch((error: any) =>
-      res.json({
-        success: false,
-        status: StatusCodes.NOT_FOUND,
-        error: {
-          code: 'SOMETHING_WENT_WRONG',
-          message: error.message,
-        },
-      }),
-    )
+export const create = (req: Request, res: OperationResponse, next: NextFunction) => {
+  return res.json({
+    success: false,
+    status: StatusCodes.NOT_IMPLEMENTED,
+    error: {
+      code: 'NOT_IMPLEMENTED',
+      message: getReasonPhrase(StatusCodes.NOT_IMPLEMENTED),
+    },
+  })
 }
