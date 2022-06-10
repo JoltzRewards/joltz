133dc31d6e2dab4f0faecf09302449a25937bce6
Joey Figaro <joey@joeyfigaro.com>
2022-06-10 12:55:03 -0400

    feat: add animations and common utilities

---
+++ packages/ui/src/animations/fade.ts
@@ -0,0 +1,11 @@
+import { keyframes } from '../stitches.config'
+
+export const _in = keyframes({
+  '0%': { opacity: 0 },
+  '100%': { opacity: 1 },
+})
+
+export const _out = keyframes({
+  '0%': { opacity: 1 },
+  '100%': { opacity: 0 },
+})
---
+++ packages/ui/src/animations/index.ts
@@ -0,0 +1,3 @@
+export * as fade from './fade'
+export * as scale from './scale'
+export * as slide from './slide'
