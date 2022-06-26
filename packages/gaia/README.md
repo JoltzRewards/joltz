963ee588904752cbe82ff016df6f191e671e22e5
Peter Nash <34408740+dartman10@users.noreply.github.com>
2022-06-26 18:46:25 -0400

    Gaia updates
    This is same code from the fork repo.  I deleted the fork too early.  :(

--- packages/gaia/README.md
+++ packages/gaia/README.md
@@ -1,1 +1,18 @@
 # @trubittech/gaia
+
+## Gaia Utilities
+
+### Basic functionalities
+
+1. Put File
+2. Get File
+3. List Files
+4. Delete File
+
+### Extended functionalities
+
+1. List Files with metadata info
+2. List Files - prefix search
+3. List Files - folders only
+4. Application controlled user data
+5. Operations involving own Gaia hub
--- packages/gaia/src/modules/list.ts
+++ packages/gaia/src/modules/list.ts
@@ -1,5 +1,52 @@
-import { Storage, StorageOperationResult } from '../types'
+import { IListFilesOptions, IListFileParam, StorageOperationResult } from '../types'
 
+/* return total number of files */
+export async function countFiles(
+  { storage }: IListFileParam,
+  _options?: IListFilesOptions,
+): Promise<StorageOperationResult<number>> {
+  return await storage
+    .listFiles((_name: string) => {
+      console.log('asdf')
+      return true /* to loop, this callback must return true per file */
+    })
+    .then((res: number) => ({
+      ok: true,
+      results: res /* file count */,
+      error: null,
+    }))
+    .catch((err: any) => ({
+      ok: false,
+      results: null,
+      error: JSON.parse(JSON.stringify(err)),
+    }))
+}
+
+/* return filenames */
+export async function listFiles(
+  { storage }: IListFileParam,
+  _options?: IListFilesOptions,
+): Promise<StorageOperationResult<string[]>> {
+  let fileList: string[] = []
+  await storage
+    .listFiles((name: string) => {
+      fileList.push(name)
+      return true /* to loop, this callback must return true per file */
+    })
+    .then((res: number) => ({
+      ok: true,
+      results: res /* not used inside this function. */,
+      error: null,
+    }))
+    .catch((err: any) => ({
+      ok: false,
+      results: null,
+      error: JSON.parse(JSON.stringify(err)),
+    }))
+  return { ok: true, results: fileList, error: null } /* fileList[] has the filenames */
+}
+
+/* dartman: I need to understand this yet.
 export async function listFiles({
   storage,
   path,
@@ -8,7 +55,7 @@ export async function listFiles({
   path: string
 }): Promise<StorageOperationResult<number>> {
   return await storage
-    .listFiles((name) => name !== undefined)
+    .listFiles((name) => name !== undefined)  //dartman: how does this behave?
     .then((ok) => ({
       ok: true,
       results: ok,
@@ -20,3 +67,4 @@ export async function listFiles({
       error: JSON.parse(JSON.stringify(err)),
     }))
 }
+*/
