d9ab3bd1d4ec7cd29586576990582380e4dcf22e
Joey Figaro <joey@joeyfigaro.com>
2022-10-07 16:52:22 -0400

    feat: add adapter and bridge typings from docs

--- packages/storage/src/types.ts
+++ packages/storage/src/types.ts
@@ -9,10 +9,32 @@ export enum StorageProviders {
 
 export interface Bridge {
   adapter: Adapter
+  isConnected: boolean
+  logstream: unknown
+  queue: unknown[]
+
+  createDirectory(opts: unknown): Promise<unknown>
+  listFiles(opts: unknown): Promise<unknown>
+  listDirectories(opts: unknown): Promise<unknown>
+  getFile(opts: unknown): Promise<unknown>
+  getDirectory(opts: unknown): Promise<unknown>
+  putFile(opts: unknown): Promise<unknown>
+  deleteDirectory(opts: unknown): Promise<unknown>
+  deleteFile(opts: unknown): Promise<unknown>
 }
 
 export interface Adapter {
   identifier: string
+  eventNames: string[]
+
+  list(opts: unknown): Promise<unknown>
+  get(opts: unknown): Promise<unknown>
+  delete(opts: unknown): Promise<unknown>
+  put(opts: unknown): Promise<unknown>
+  replace(opts: unknown): Promise<unknown>
+
+  connect(opts: unknown): Promise<unknown>
+  disconnect(opts: unknown): Promise<void>
 }
 
 // export interface TBDAdapter extends Adapter {}
